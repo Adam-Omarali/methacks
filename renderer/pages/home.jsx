4caeab9fa1d2238ccebdd0aa8920809626c35659
Adam <adamomarali37@gmail.com>
2023-05-06 12:39:40 -0400

    change instrument

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -8,20 +8,20 @@ function Home() {
 
   const TWINKLE_TWINKLE = {
     notes: [
-      {pitch: 60, startTime: 0.0, endTime: 0.5, program: "50"},
-      {pitch: 60, startTime: 0.5, endTime: 1.0, program: "50"},
-      {pitch: 67, startTime: 1.0, endTime: 1.5, program: "50"},
-      {pitch: 67, startTime: 1.5, endTime: 2.0, program: "50"},
-      {pitch: 69, startTime: 2.0, endTime: 2.5, program: "50"},
-      {pitch: 69, startTime: 2.5, endTime: 3.0, program: "50"},
-      {pitch: 67, startTime: 3.0, endTime: 4.0, program: "50"},
-      {pitch: 65, startTime: 4.0, endTime: 4.5, program: "50"},
-      {pitch: 65, startTime: 4.5, endTime: 5.0, program: "50"},
-      {pitch: 64, startTime: 5.0, endTime: 5.5, program: "50"},
-      {pitch: 64, startTime: 5.5, endTime: 6.0, program: "50"},
-      {pitch: 62, startTime: 6.0, endTime: 6.5, program: "50"},
-      {pitch: 62, startTime: 6.5, endTime: 7.0, program: "50"},
-      {pitch: 60, startTime: 7.0, endTime: 8.0, program: "50"},
+      {pitch: 60, startTime: 0.0, endTime: 0.5},
+      {pitch: 60, startTime: 0.5, endTime: 1.0},
+      {pitch: 67, startTime: 1.0, endTime: 1.5},
+      {pitch: 67, startTime: 1.5, endTime: 2.0},
+      {pitch: 69, startTime: 2.0, endTime: 2.5},
+      {pitch: 69, startTime: 2.5, endTime: 3.0},
+      {pitch: 67, startTime: 3.0, endTime: 4.0},
+      {pitch: 65, startTime: 4.0, endTime: 4.5},
+      {pitch: 65, startTime: 4.5, endTime: 5.0},
+      {pitch: 64, startTime: 5.0, endTime: 5.5},
+      {pitch: 64, startTime: 5.5, endTime: 6.0},
+      {pitch: 62, startTime: 6.0, endTime: 6.5},
+      {pitch: 62, startTime: 6.5, endTime: 7.0},
+      {pitch: 60, startTime: 7.0, endTime: 8.0},
     ],
     totalTime: 8
   };
@@ -31,10 +31,10 @@ function Home() {
 
   useEffect(() => {
     // const player = new core.Player();
-    let rnnPlayer = new core.Player();
+    // let rnnPlayer = new core.Player();
     // rnnPlayer.start(TWINKLE_TWINKLE);
 
-    rnnPlayer = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
+    let rnnPlayer = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
     rnnPlayer.setTempo(100);
 
     let musicR = new music_rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
@@ -52,7 +52,10 @@ function Home() {
       const qns = core.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
       musicR
       .continueSequence(qns, rnn_steps, rnn_temperature)
-      .then((sample) => rnnPlayer.start(sample));
+      .then((sample) => {
+        sample.notes.forEach(n => n.program = 36);
+        rnnPlayer.start(sample);
+      });
     }
 
     play();
