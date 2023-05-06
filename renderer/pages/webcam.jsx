0b7e87bc3ed04c370f0dbf2dbd1b165d99cfc85f
Léo Liu <leoliuhy@gmail.com>
2023-05-06 14:25:54 -0400

    gesture recognition of emojis

--- renderer/pages/webcam.jsx
+++ renderer/pages/webcam.jsx
@@ -11,25 +11,26 @@ import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
-import { drawHand } from "../utilities"; 
+import { drawHand } from "../utilities";
 
 import * as fp from "fingerpose";
-// import victory from "./victory.png"; // TODO
-// import thumbs_up from "./thumbs_up.png"; // TODO
 
 function Camrecognize() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const [cameraOn, setCameraOn] = useState(false);
+  const [emoji, setEmoji] = useState(null);
+  const images = { thumbs_up: "/images/thumbs_up.png", victory: "/images/victory.png" };
+
 
   const runHandpose = async () => {
     const net = await handpose.load()
     console.log("[+] handpose is loaded")
-    setInterval(() => {detect(net)}, 500)
+    setInterval(() => { detect(net) }, 500)
   }
 
   const detect = async (net) => {
-    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState===4){
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
       const video = webcamRef.current.video;
       const videoWidth = video.videoWidth;
       const videoHeight = video.videoHeight;
@@ -40,7 +41,23 @@ function Camrecognize() {
       canvasRef.current.height = videoHeight;
 
       const hand = await net.estimateHands(video);
-      console.log(hand);
+
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator(
+          [
+            fp.Gestures.VictoryGesture,
+            fp.Gestures.ThumbsUpGesture
+          ]
+        )
+
+        const gesture = await GE.estimate(hand[0].landmarks, 8)
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          const confidence = gesture.gestures.map((prediction) => prediction.score);
+          const maxConfidence = confidence.indexOf(Math.max.apply(null, confidence));
+          setEmoji(gesture.gestures[maxConfidence].name);
+          console.log(emoji)
+        }
+      }
 
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
@@ -84,7 +101,23 @@ function Camrecognize() {
           }}
         />}
 
-        {/* NEW STUFF */}
+        {emoji !== null ? (
+          <img
+            src={images[emoji]}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 400,
+              bottom: 500,
+              right: 0,
+              textAlign: "center",
+              height: 100,
+            }}
+          />
+        ) : (
+          ""
+        )}
 
         <button className='btn-blue' onClick={() => setCameraOn(!cameraOn)} style={{
           position: 'absolute',
