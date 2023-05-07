fe2eb0f702d6cd30e2f7dc397bffb3dc4116e116
Jerry Zhao <jerryzhao@Jerrys-MacBook-Air.local>
2023-05-06 23:59:38 -0400

    harmony

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -43,12 +43,18 @@ function Home() {
       {pitch: 62, startTime: 6.5, endTime: 7.0, isDrum: true},
       {pitch: 60, startTime: 7.0, endTime: 8.0, isDrum: true},
     ],
-    // quantizationInfo: {stepsPerQuarter: 4},
-    // tempos: [{time: 0, qpm: 120}],
+    quantizationInfo: {stepsPerQuarter: 4},
+    tempos: [{time: 0, qpm: 60}],
     totalQuantizedSteps: 8
   };
+
+  let fontSelection = [[40,41,42,43],[68,73,60,70],[56,59,57,58]];
+
+  let avoidChord = [2,6,8];
+
+  let fontIdx = 0;
   
-  const rnn_steps = 100;
+  const rnn_steps = 200;
   const rnn_temperature = 1.5;
 
   useEffect(() => {
@@ -57,7 +63,7 @@ function Home() {
     melodyPlayer.initialize();
 
     let rnnPlayer = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
-    rnnPlayer.setTempo(60);
+    rnnPlayer.setTempo(80);
 
     let drumPlayer = new music_rnn.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/drum_kit_rnn');
     drumPlayer.initialize();
@@ -65,82 +71,130 @@ function Home() {
     let rnnDrum = new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/jazz_kit');
     // rnnDrum.setTempo(60);
 
-    let player = new core.Player();
-    player.start(DRUMS);
+    // let player = new core.Player();
+    // player.start(DRUMS);
 
 
-function play2() {
-  if (rnnDrum.isPlaying()) {
-    rnnDrum.stop();
-    return;
-  }
+// function play2() {
+//   if (rnnDrum.isPlaying()) {
+//     rnnDrum.stop();
+//     return;
+//   }
   
-  const qns = core.sequences.quantizeNoteSequence(DRUMS, 4);
-  console.log(qns)
-  drumPlayer
-  .continueSequence(qns, rnn_steps, rnn_temperature)
+//   const qns = core.sequences.quantizeNoteSequence(DRUMS, 4);
+//   console.log(qns)
+//   drumPlayer
+//   .continueSequence(qns, rnn_steps, rnn_temperature)
   // .then((sample) => {
   //   rnnDrum.start(sample);
   //   // console.log(sample)
   // });
-}
+// }
   
           
   // melody generation
 
-  function play() {
-    if (rnnPlayer.isPlaying()) {
-      rnnPlayer.stop();
-      return;
-    }
+function play() {
+  if (rnnPlayer.isPlaying()) {
+    rnnPlayer.stop();
+    return;
+  }
   
   const qns = core.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
   console.log(qns)
-  // melodyPlayer
-  // .continueSequence(qns, rnn_steps, rnn_temperature)
-  // .then((sample) => {
-  //   sample.notes.forEach(n => n.program = 0);
-  //   sample.notes.forEach(n => n.endTime += 8);
-  //   // console.log(sample)
-  //   let finalSample = [];
-  //   sample.notes.map((n, idx) => {
-  //     finalSample.push(n)
+  melodyPlayer
+  .continueSequence(qns, rnn_steps, rnn_temperature)
+  .then((sample) => {
+    sample.notes.forEach(n => n.program = fontSelection[fontIdx][0], n => n.pitch+=3);
+    // console.log(sample)
+    let finalSample = [];
+    sample.notes.map((n, idx) => {
+      finalSample.push(n)
 
-  //     if(n.quantizedStartStep % 8 == 0){
+      if(n.quantizedStartStep % 2 == 0){
+        var flag = false;
+        let newNote0;
+        avoidChord.forEach(i => flag &= (Math.abs(n.pitch - 5) % i != 0))
+        if(!flag){
+          newNote0 = {
+            pitch: n.pitch-=5,
+            quantizedStartStep: sample.notes[idx].quantizedStartStep,
+            quantizedEndStep: sample.notes[idx].quantizedEndStep+=1,
+            program: n.program = fontSelection[fontIdx][1],
+            performance: n.velocity-=20
+          }
+        }else{
+          newNote0 = {
+            pitch: n.pitch-=8,
+            quantizedStartStep: sample.notes[idx].quantizedStartStep,
+            quantizedEndStep: sample.notes[idx].quantizedEndStep+=1,
+            program: n.program = fontSelection[fontIdx][1],
+            performance: n.velocity-=20
+          }
+        }
 
-  //       let newNote1 = {
-  //         pitch: n.pitch-=16,
-  //         quantizedStartStep: sample.notes[idx].quantizedStartStep,
-  //         quantizedEndStep: sample.notes[idx].quantizedStartStep + 8, 
-  //         program: n.program = 19}
-  //       finalSample.push(newNote1)
+      finalSample.push(newNote0)
+    }
 
-  //       let newNote2 = {
-  //         pitch: n.pitch+=8,
-  //         quantizedStartStep: sample.notes[idx].quantizedStartStep,
-  //         quantizedEndStep: sample.notes[idx].quantizedStartStep + 16, 
-  //         program: n.program = 73}
-  //       finalSample.push(newNote2)
-  //     }
+    if(n.quantizedStartStep % 8 == 0){
 
-  //     // if (n.quantizedStartStep % 2 == 0) {
-  //     //   let newNote3 = {
-  //     //     pitch: n.pitch-=16,
-  //     //     quantizedStartStep: sample.notes[idx].quantizedStartStep,
-  //     //     quantizedEndStep: sample.notes[idx].quantizedStartStep + 2, 
-  //     //     program: n.program = "drums",
-  //     //     isDrum: n.isDrum=true};
-  //     //   finalSample.push(newNote3)
-  //     // }
+      var flag = false;
+        let newNote1;
+        avoidChord.forEach(i => flag &= (Math.abs(n.pitch - 13) % i != 0))
+        if(!flag){
+          newNote1 = {
+            pitch: n.pitch-=13,
+            quantizedStartStep: sample.notes[idx].quantizedStartStep,
+            quantizedEndStep: sample.notes[idx].quantizedEndStep+=8,
+            program: n.program = fontSelection[fontIdx][2],
+            performance: n.velocity-=20
+          }
+        }else{
+          newNote1 = {
+            pitch: n.pitch-=16,
+            quantizedStartStep: sample.notes[idx].quantizedStartStep,
+            quantizedEndStep: sample.notes[idx].quantizedEndStep+=8,
+            program: n.program = fontSelection[fontIdx][2],
+            performance: n.velocity-=20
+          }
+        }
+        finalSample.push(newNote1)
+    }
 
-  //   });
-  //   sample.notes = finalSample;
-  //   rnnPlayer.start(sample);
-  // });
+    if (n.quantizedStartStep % 16 == 0) {
+      let newNote2 = {
+        pitch: n.pitch-=16,
+        quantizedStartStep: sample.notes[idx].quantizedStartStep,
+        quantizedEndStep: sample.notes[idx].quantizedStartStep + 16,
+        program: n.program = fontSelection[fontIdx][3],
+        performance: n.velocity-=30}
+        finalSample.push(newNote2)
+    }
+
+      // let drumNote1 = {
+      //   pitch: n.pitch=50,
+      //   quantizedStartStep: n.startTime,
+      //   quantizedEndStep: n.startTime+=2,
+      //   program: n.program = "drums",
+      //   isDrum: n.isDrum=true};
+      //   finalSample.push(drumNote1)
+
+        // let drumNote2 = {
+        //   pitch: n.pitch=50,
+        //   quantizedStartStep: quantizedStartStep,
+        //   quantizedEndStep: quantizedStartStep + 1, 
+        //   program: n.program = "drums",
+        //   isDrum: n.isDrum=true};
+        // finalSample.push(drumNote2)
+
+    });
+    sample.notes = finalSample;
+    rnnPlayer.start(sample);
+    // rnnPlayer.start(DRUMS);
+  });
 }
 
   play();
-  play2();
 
 
   }, [])
